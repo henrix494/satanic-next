7cba596003afcf9543ab883962f3fee1eb59ee1e
henrix494 <henrix494@gmail.com>
2023-12-01 18:10:58 +0200

    test staff

---
+++ app/api/create-pets-table/route.ts
@@ -0,0 +1,17 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    // Replace 'natan' and 'kov' with the actual names of the tables you want to keep
+    const tablesToKeep = ["poster_images", "poster_sizes", "posters"];
+
+    // Fetch all table names from the database
+    const result =
+      await sql`SELECT Image_url FROM poster_images,posters WHERE posters._id = poster_images.Poster_id`;
+    console.log(result.rows);
+    return NextResponse.json({ success: result.rows }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 export default function Home() {
-  return <main></main>;
+  const [test, setTest] = useState();
+  useEffect(() => {
+    const testData = async () => {
+      const data = await fetch("http://localhost:3000/api/create-pets-table");
+      const res = await data.json();
+      setTest(res.success[0].image_url);
+    };
+    testData();
+  }, []);
+  return (
+    <main>
+      {test ? <Image src={test} width={100} height={100} /> : "Loading"}
+    </main>
+  );
 }
